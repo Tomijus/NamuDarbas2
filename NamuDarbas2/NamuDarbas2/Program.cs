e7a655ee0b41f6d5c181333e648fe8ff1e86b62e
TomasG <tomijus7@gmail.com>
2019-12-19 15:34:50 +0200

    NamuDarbas2 project started

---
+++ NamuDarbas2/NamuDarbas2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NamuDarbas2
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+
+        static List<int> AtiduokSkaiciuLista(int skaicius)
+        {
+
+        }
+    }
+}
