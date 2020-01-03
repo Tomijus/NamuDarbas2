23f85e5c7d52ffbaa321630875f86a6e7f93eff5
TomasG <tomijus7@gmail.com>
2020-01-03 14:36:08 +0200

    Added AtiduokSkaiciuLista function

--- NamuDarbas2/NamuDarbas2/Program.cs
+++ NamuDarbas2/NamuDarbas2/Program.cs
@@ -7,12 +7,19 @@ namespace NamuDarbas2
     {
         static void Main()
         {
-            
+            AtiduokSkaiciuLista(123456);
         }
 
         static List<int> AtiduokSkaiciuLista(int skaicius)
         {
-
+            List<int> intList = new List<int>();
+            string strSkaicius = Convert.ToString(skaicius);
+            foreach (char item in strSkaicius)
+            {
+                skaicius = Convert.ToInt32(item);
+                intList.Add(skaicius);
+            }
+            return intList;
         }
     }
 }
