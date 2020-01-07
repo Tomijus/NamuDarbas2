c943d88af7719031cb7654ecf74f38127a787f1e
TomasG <tomijus7@gmail.com>
2020-01-07 11:40:25 +0200

    Foreach changed to For and some small cleanup

--- NamuDarbas2/NamuDarbas2/Program.cs
+++ NamuDarbas2/NamuDarbas2/Program.cs
@@ -10,7 +10,7 @@ namespace NamuDarbas2
             for (int i = 100000; i <= 999999; i++)
             {
 
-                if (IsAllElementsUnique(AtiduokSkaiciuLista(i)) && SameCharsDifferentPlaces(AtiduokSkaiciuLista(i), AtiduokSkaiciuLista(i * 2)) && SameCharsDifferentPlaces(AtiduokSkaiciuLista(i), AtiduokSkaiciuLista(i * 3)) && SameCharsDifferentPlaces(AtiduokSkaiciuLista(i), AtiduokSkaiciuLista(i * 4)) && SameCharsDifferentPlaces(AtiduokSkaiciuLista(i), AtiduokSkaiciuLista(i * 5)) && SameCharsDifferentPlaces(AtiduokSkaiciuLista(i), AtiduokSkaiciuLista(i * 6)))
+                if (IsAllElementsUnique(GiveBackIntList(i)) && SameCharsDifferentPlaces(GiveBackIntList(i), GiveBackIntList(i * 2)) && SameCharsDifferentPlaces(GiveBackIntList(i), GiveBackIntList(i * 3)) && SameCharsDifferentPlaces(GiveBackIntList(i), GiveBackIntList(i * 4)) && SameCharsDifferentPlaces(GiveBackIntList(i), GiveBackIntList(i * 5)) && SameCharsDifferentPlaces(GiveBackIntList(i), GiveBackIntList(i * 6)))
                 {
                     Console.WriteLine("Magic number: " + i);
                     Console.WriteLine("Magic number multiplied by 2: " + i * 2);
@@ -22,20 +22,17 @@ namespace NamuDarbas2
                 }
             }
         }
-
-        static List<int> AtiduokSkaiciuLista(int skaicius)
+        static List<int> GiveBackIntList(int skaicius)
         {
             List<int> intList = new List<int>();
             string strSkaicius = Convert.ToString(skaicius);
             foreach (char item in strSkaicius)
             {
                 skaicius = int.Parse(item.ToString());
-                //Console.WriteLine(skaicius);
                 intList.Add(skaicius);
             }
             return intList;
         }
-
         static bool IsAllElementsUnique(List<int> listas)
         {
             List<int> checkList = new List<int>();
@@ -50,7 +47,6 @@ namespace NamuDarbas2
                 }
                 if (checkList.Count > 1)
                 {
-                    //Console.WriteLine("Duplicate found!");
                     return false;
                 }
                 else
@@ -58,34 +54,28 @@ namespace NamuDarbas2
                     checkList.Clear();
                 }
             }
-            //Console.WriteLine("All are unique!");
             return true;
         }
-
         static bool SameCharsDifferentPlaces(List<int> pirmasListas, List<int> antrasListas)
         {
             List<int> checkList = new List<int>();
-            int pirmasListIndex = 0;
-            foreach (int item in pirmasListas)
+            for (int j = 0; j < pirmasListas.Count; j++)
             {
                 for (int i = 0; i < antrasListas.Count; i++)
                 {
-                    if (item == antrasListas[i])
+                    if (pirmasListas[j] == antrasListas[i])
                     {
-                        if (pirmasListIndex != antrasListas.IndexOf(antrasListas[i]))
+                        if (pirmasListas.IndexOf(pirmasListas[j]) != antrasListas.IndexOf(antrasListas[i]))
                         {
                             checkList.Add(1);
                         }
                     }
                 }
-                pirmasListIndex++;
             }
             if (checkList.Count == 6)
             {
-                //Console.WriteLine("Visi elementai skirtingi ir yra skirtingose vietose!");
                 return true;
             }
-            //Console.WriteLine("Listai neatitinka salygu!");
             return false;
         }
     }
